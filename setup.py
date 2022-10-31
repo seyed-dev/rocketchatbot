c81c87fd0c41b5a70329e681244e6b5971c49062
seyed <mohamad.khoshnava@gmail.com>
2022-10-31 15:22:33 +0330

    fix name

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(
-    name="rocketpybot",
+    name="pyrocketbot",
     version="0.0.1",
     url="https://github.com/seyed-dev/rocketpybot",
     license="MIT",
     author="SeYeD Mohammad Khoshnava | seyed.dev",
     author_email="me@seyed.dev",
-    description="Python lib for create rocketchat bot with getupdate like TelegramBots",
+    description="Python lib for create rocket.chat bot with getupdate like TelegramBots",
     long_description=open("README.md", "r").read(),
     long_description_content_type="text/markdown",
     install_requires=("rocketchat-API",),
